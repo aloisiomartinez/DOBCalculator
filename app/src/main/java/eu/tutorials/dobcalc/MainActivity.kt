328ba2788393377705df8ef4540dba1afcd80697
Aloisio Martinez <aloisiojacobs36@gmail.com>
2022-09-18 00:18:17 -0300

    datePickerDialog added

--- app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
@@ -1,11 +1,42 @@
 package eu.tutorials.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
     }
+
+    private fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{
+                view, year, month, dayofMonth ->
+
+                Log.i("ButtonClicked", "Bota clicado")
+                Toast.makeText(this, "DatePicker works", Toast.LENGTH_LONG).show()
+            }, year, month, day
+        ).show()
+
+
+    }
+
+
 }
