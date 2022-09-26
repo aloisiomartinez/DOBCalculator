7d77244dd5cc27f0bfe50245203433db2f8e2090
Aloisio Martinez <aloisiomartinez@pop-os.localdomain>
2022-09-26 07:49:32 -0300

    date selected now show in TextView

--- app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
+++ app/src/main/java/eu/tutorials/dobcalc/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
-
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
         btnDatePicker.setOnClickListener {
             clickDatePicker()
         }
@@ -23,20 +28,23 @@ class MainActivity : AppCompatActivity() {
     private fun clickDatePicker() {
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
-        val month = myCalendar.get(Calendar.MONTH)
+        val month: Int = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{
-                view, year, month, dayofMonth ->
+           { view, selectedYear, selectedMonth, selectedDayofMonth ->
+                Toast.makeText(this, "Year was $selectedYear, month was ${selectedMonth+1}, day was $selectedDayofMonth+", Toast.LENGTH_LONG).show()
 
-                Log.i("ButtonClicked", "Bota clicado")
-                Toast.makeText(this, "DatePicker works", Toast.LENGTH_LONG).show()
+               val selectedDate = "$selectedDayofMonth/${selectedMonth+1}/$selectedYear"
+
+               tvSelectedDate?.text = selectedDate
+
+               val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+               val theDate = sdf.parse(selectedDate)
             }, year, month, day
+        
+
         ).show()
-
-
     }
-
-
 }
